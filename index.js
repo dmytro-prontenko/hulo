37120905a73995367fad591f59db005839c5c9fd
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-07 14:05:07 +0100

    swiper added

---
+++ index.js
@@ -0,0 +1,13 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
+
+
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css/bundle';
+
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+
+
+});
